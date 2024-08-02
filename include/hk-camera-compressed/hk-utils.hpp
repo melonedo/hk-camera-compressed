e9f651a21aad73d18d9488b8742d5fc64365a917
melonedo <44501064+melonedo@users.noreply.github.com>
2024-08-02 20:24:52 +0800

    Fix .gitignore and .gitkeep

--- include/hk-camera-compressed/hk-utils.hpp
+++ include/hk-camera-compressed/hk-utils.hpp
@@ -3,7 +3,3 @@
 
 bool PrintDeviceInfo(const MV_CC_DEVICE_INFO *pstMVDevInfo);
 const char *GetCameraName(const MV_CC_DEVICE_INFO *pstMVDevInfo);
-enum {
-  MV_ADC_BIT_DEPTH_8 = 0,
-  MV_ADC_BIT_DEPTH_12 = 3,
-};
